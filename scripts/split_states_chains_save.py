262cf34b992aa8ac748711a1c3090c84886504f0
Aliza Rubenstein <aliza.rubenstein@gmail.com>
2016-06-02 13:45:39 -0400

    Fixed complex files - they only had chain A for some reason, now they have both chains Also fixed error in split_states_chains_save.py where I had mistakenly deleted split_states line

--- scripts/split_states_chains_save.py
+++ scripts/split_states_chains_save.py
@@ -5,11 +5,13 @@ def split_states_chains_save( sel1 ):
     Simple script to split states of NMR ensemble and then save the complex and chain A separately for each state
     '''
 
+    cmd.split_states(sel1)
+
     for ind in range(1,cmd.count_states(sel1)+1):
         if cmd.count_states(sel1) > 1:
             file_prefix="{0}_{1:04d}".format(sel1, ind)
         else:
-            file_prefix=sel1    
+            file_prefix=sel1   
         cmd.save(file_prefix + "_complex.pdb", file_prefix)
         cmd.split_chains(file_prefix)
         cmd.save(file_prefix + ".pdb", file_prefix + "_A")
