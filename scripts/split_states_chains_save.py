0167db332e8d7f0268179af9945dc46774e03af7
Aliza Rubenstein <aliza.rubenstein@gmail.com>
2016-06-02 11:54:13 -0400

    Added pdbs for all NMR states for 1N7T, 1vj6, and 2k20 (separated into complex and single-chain A models) Added pymol script to separate states and chains and save them

---
+++ scripts/split_states_chains_save.py
@@ -0,0 +1,15 @@
+from pymol import cmd, stored
+
+def split_states_chains_save( sel1 ):
+    '''
+    Simple script to split states of NMR ensemble and then save the complex and chain A separately for each state
+    '''
+    cmd.split_states(sel1)
+    
+    for ind in range(1,cmd.count_states(sel1)+1):
+        file_prefix="{0}_{1:04d}".format(sel1, ind)
+        cmd.save(file_prefix + "_complex.pdb", file_prefix)
+        cmd.split_chains("{0}_{1:04d}".format(sel1,ind))
+        cmd.save(file_prefix + ".pdb", file_prefix + "_A")
+
+cmd.extend( "split_states_chains_save", split_states_chains_save );
