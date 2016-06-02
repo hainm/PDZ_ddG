bc45556a51142d2fad3699584575cf5b6c20fa8f
Aliza Rubenstein <aliza.rubenstein@gmail.com>
2016-06-02 13:07:22 -0400

    Added 1be9 and 1qav input pdbs (complex and chain A). Modified split_states_chains_save.py to work well with non-NMR ensembles

--- scripts/split_states_chains_save.py
+++ scripts/split_states_chains_save.py
@@ -4,12 +4,14 @@ def split_states_chains_save( sel1 ):
     '''
     Simple script to split states of NMR ensemble and then save the complex and chain A separately for each state
     '''
-    cmd.split_states(sel1)
-    
-    for ind in range(1,cmd.count_states(sel1)+1):
-        file_prefix="{0}_{1:04d}".format(sel1, ind)
-        cmd.save(file_prefix + "_complex.pdb", file_prefix)
-        cmd.split_chains("{0}_{1:04d}".format(sel1,ind))
-        cmd.save(file_prefix + ".pdb", file_prefix + "_A")
 
+    for ind in range(1,cmd.count_states(sel1)+1):
+        if cmd.count_states(sel1) > 1:
+            file_prefix="{0}_{1:04d}".format(sel1, ind)
+        else:
+            file_prefix=sel1    
+        cmd.save(file_prefix + "_complex.pdb", file_prefix)
+        cmd.split_chains(file_prefix)
+        cmd.save(file_prefix + ".pdb", file_prefix + "_A")
+        
 cmd.extend( "split_states_chains_save", split_states_chains_save );
