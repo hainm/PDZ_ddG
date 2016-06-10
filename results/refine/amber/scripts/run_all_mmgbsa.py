edd88fff1dd03db3c0c1419c6f3c7c8b806d36f4
Hai Nguyen <hainm.comp@gmail.com>
2016-06-10 14:05:58 -0400

    update mmgbsa scripts

--- results/refine/amber/scripts/run_all_mmgbsa.py
+++ results/refine/amber/scripts/run_all_mmgbsa.py
@@ -6,7 +6,13 @@ SCRIPT_PATH = '/project1/dacase-001/haichit/rosseta_amber/scripts/elf1'
 sys.path.append(SCRIPT_PATH)
 from utils import temp_change_dir
 
-pdblist = open('pdblist.txt').read().split()
+try:
+    pdblist = sys.argv[1]
+except IndexError:
+    pdblist = 'pdblist.txt'
+
+print("reading {}".format(pdblist))
+pdblist = open(pdblist).read().split()
 
 # update scripts/run_mmgbsa.py if you want to change mmgbsa.in
 run_script = os.path.abspath('scripts/run_mmgbsa.py')
